34300a47956f38a40dc55c78ce844e5d770852fd
osswangxining <osswangxining@163.com>
2017-12-26 21:05:51 +0800

    enable Refresh

--- spring-cloud-config-sample/src/main/java/sample/Application.java
+++ spring-cloud-config-sample/src/main/java/sample/Application.java
@@ -4,6 +4,7 @@ package sample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@RefreshScope
 public class Application {
 
 	@Autowired
--- spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/ConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/ConfigServerApplication.java
@@ -3,10 +3,12 @@ package org.springframework.cloud.config.server;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
+@RefreshScope
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
